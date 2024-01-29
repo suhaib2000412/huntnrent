da78fe484f4247219ef3e7ba0603448f10f2b5df
suhaib <suhaib2000412@gmail.com>
2024-01-29 16:33:35 +0530

    first commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,15 @@
+
+    <!-- Required Js -->
+    <script src=<?php base_url()?>"assets/js/vendor-all.min.js"></script>
+    <script src=<?php base_url()?>"assets/js/plugins/bootstrap.min.js"></script>
+    <script src=<?php base_url()?>"assets/js/pcoded.min.js"></script>
+
+<!-- Apex Chart -->
+<script src=<?php base_url()?>"assets/js/plugins/apexcharts.min.js"></script>
+
+
+<!-- custom-chart js -->
+<script src=<?php base_url()?>"assets/js/pages/dashboard-main.js"></script>
+</body>
+
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Admin Panel</h1>
+
+	
+</div>
+
+</body>
+</html>
